605a2b383046d90a4f3e89be209582ddde6c8675
epotignano <epotignano@gmail.com>
2016-02-09 23:11:43 -0300

    Progreso para usar react-redux router

---
+++ js/containers/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by epotignano on 09/02/16.
+ */
+
+export {default as App} from "./App";
--- js/index.js
+++ js/index.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistory, routeReducer } from 'react-router-redux'
+import reducers from './reducers'
 import 'materialize-css';
 import 'materialize-js';
-import { Router, Route, Link, browserHistory} from "react-router";
+import { App } from "./containers/index";
+
+const reducer = combineReducers(Object.assign({}, reducers, {
+  routing: routeReducer
+}))
+
+// Sync dispatched route actions to the history
+const reduxRouterMiddleware = syncHistory(browserHistory)
+const createStoreWithMiddleware = applyMiddleware(reduxRouterMiddleware)(createStore)
+
+const store = createStoreWithMiddleware(reducer)
+
+// Required for replaying actions from devtools to work
+reduxRouterMiddleware.listenForReplays(store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('main')
+)
 
 
-var About = React.createClass({
-  render() {
-    return (<div>
-      <h1>Ay vdai</h1>
-    </div>)
-  }
-});
 
 
-
-ReactDOM.render((<Router history={browserHistory}>
-    <Route path="/" component={Home}>
-      <Route path="about" component={About}></Route>
-    </Route>
-  </Router>
-), document.getElementById('main'));
